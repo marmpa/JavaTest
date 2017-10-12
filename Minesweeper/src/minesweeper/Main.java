68c14adb2f8d7ee0950eded33afdb8c988d3ae66
marios <marios@Zcomputer>
2017-10-12 13:43:32 +0300

    Basic Testing

--- Minesweeper/src/minesweeper/Main.java
+++ Minesweeper/src/minesweeper/Main.java
@@ -11,11 +11,9 @@ package minesweeper;
  */
 public class Main {
 
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Test");
     }
     
 }
